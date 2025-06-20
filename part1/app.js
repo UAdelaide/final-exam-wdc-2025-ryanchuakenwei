f238757aa0e25d702e1c5f60cdb506f925555944
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:08:42 +0000

    [automatic commit] 2025-06-20 12:08:42 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mysql = require('mysql2/promise');
+const app = express();
+const PORT = process.env.PORT || 3000;
