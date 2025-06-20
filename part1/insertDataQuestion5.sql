3c9f337e8259714976e5639b29bd0ad91d4dccd4
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 11:45:53 +0000

    [automatic commit] 2025-06-20 11:45:53 UTC

--- part1/insertDataQuestion5.sql
+++ part1/insertDataQuestion5.sql
@@ -12,4 +12,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 INSERT INTO Dogs (name, size, owner_id) VALUES
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
 ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
-('Humzah', 'large', )
+('Humzah', 'large', (SELECT user_id FROM Users WHERE username = '')
