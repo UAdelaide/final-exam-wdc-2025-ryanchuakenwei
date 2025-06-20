468b48f6e28ae366f23e8ccd49f3f9c6aa9dde24
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 11:34:56 +0000

    [automatic commit] 2025-06-20 11:34:56 UTC

--- part1/insertDataQuestion5.sql
+++ part1/insertDataQuestion5.sql
@@ -1,4 +1,4 @@
 USE DogWalkService;
 
 INSERT INTO Users (username, email, password_hash, role) VALUES
-('alice123', 'alice@example.com', 'hashed'
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
