8b7a54815e0a806e9ceacc900350bc2b29b2daac
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 14:11:13 +0000

    [automatic commit] 2025-06-20 14:11:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send(`
-        `)
+// app.get('/', (req, res) => {
+//     res.send(`
+//         `)
 
 let db;
 (async () => {
