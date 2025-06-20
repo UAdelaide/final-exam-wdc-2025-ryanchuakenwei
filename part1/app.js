736f874512463ba4729728b45c3a69ebae4b1ffc
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:55:18 +0000

    [automatic commit] 2025-06-20 12:55:18 UTC

--- part1/app.js
+++ part1/app.js
@@ -163,5 +163,7 @@ app.get ('api/walkers/summary', async (req, res) => {
             SELECT u.username,
             COUNT(wr.rating_id) AS total_ratings,
             AVG(wr.rating) AS average_rating
-            COUNT(DISTINCT CASE WHEN wa.status = 'accepted' THEN wa.request_id END) AS com_walks`)
+            COUNT(DISTINCT CASE WHEN wa.status = 'accepted' THEN wa.request_id END) AS completed_walks
+            FROM Users u
+            LEFT JO`)
     }
