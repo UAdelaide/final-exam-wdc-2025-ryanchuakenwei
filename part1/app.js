f0295478c84118f02d8af757d0799b91314ea4cc
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 14:01:16 +0000

    [automatic commit] 2025-06-20 14:01:16 UTC

--- part1/app.js
+++ part1/app.js
@@ -101,7 +101,7 @@ let db;
             `);
 
             await db.execute(`
-                INSERT INTO Dogs (owner_id, name, size) VALUES
+                INSERT INTO Dogs (name, size, owner_id) VALUES
                 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
                 ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
                 ('Humzah', 'large', (SELECT user_id FROM Users WHERE username = 'aiden123')),
