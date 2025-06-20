e2307b3b90d019f5e6e3fc56148d84d394e7ea33
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:27:07 +0000

    [automatic commit] 2025-06-20 12:27:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -82,4 +82,6 @@ let db;
 
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
         if (rows[0].count === 0) {
-            await db.execute(``)
+            await db.execute(`
+                INSERT INTO USERS (username, email, password_hash, role) VALUES
+                `)
