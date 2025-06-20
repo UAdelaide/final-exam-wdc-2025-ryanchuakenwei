1af85ec789855b15d3190bcac6b5faeede700d0f
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:42:08 +0000

    [automatic commit] 2025-06-20 12:42:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -142,5 +142,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('api/walkreqests/open', async (req, res) => {
     try {
         const [requests] = await db.execute(`
-            SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr`)
+            SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
+            FROM WalkRequests wr
+            `)
     })
