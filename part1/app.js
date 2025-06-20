3ed28308febc251cee50651aef1dde9ec171cb33
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:17:29 +0000

    [automatic commit] 2025-06-20 12:17:28 UTC

--- part1/app.js
+++ part1/app.js
@@ -25,5 +25,8 @@ let db;
     await db.execute(`
         CREATE TABLE IF NOT EXISTS Users (
           user_id INT AUTO_INCREMENT PRIMARY KEY,
-          username VARCHAR(255) NOT NULL
+          username VARCHAR(255) UNIQUE NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL,
+          password_hash VARCHAR(255) NOT NULL,
+          
         )`);
