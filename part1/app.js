e584e0d97fbf9b0188a5310db9f6daaf773a3098
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:39:01 +0000

    [automatic commit] 2025-06-20 12:39:01 UTC

--- part1/app.js
+++ part1/app.js
@@ -129,5 +129,10 @@ app.get('/api/dogs', async (req, res) => {
             FROM Dogs d
             JOIN Users u ON d.owner_id =u.user_id
             `);
-            
+            res.json(dogs);
+        }
+        catch (err) {
+            console.error('Error fetching dogs:', err);
+            res.status(500).json({ error: 'Failed to fetch dogs' });
+        }
     }
