e7b50619655309fcea55ca6f4a71bb94386243bb
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 13:39:16 +0000

    [automatic commit] 2025-06-20 13:39:16 UTC

--- part1/app.js
+++ part1/app.js
@@ -163,7 +163,7 @@ app.get('api/walkreqests/open', async (req, res) => {
                 location: request.location,
                 owner_username: request.owner_username
             }));
-            res.json();
+            res.json(requestDisplay);
         }
         catch (err) {
             console.error('Error fetching open walk requests:', err);
