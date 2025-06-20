eb6e1164c1df2a6b788cb1f267365e215a71d51c
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:29:37 +0000

    [automatic commit] 2025-06-20 12:29:37 UTC

--- part1/app.js
+++ part1/app.js
@@ -102,4 +102,10 @@ let db;
 
             await db.execute(`
                 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-                
+                ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+                ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+                ((SELECT dog_id FROM Dogs WHERE name = 'Humzah'), '2025-06-10 11:00:00', 30, 'Parklands', 'open'),
+                ((SELECT dog_id FROM Dogs WHERE name = 'Big Fella'), '2025-06-10 12:00:00', 30, 'Beachside Ave', 'accepted'),
+                ((SELECT dog_id FROM Dogs WHERE name = 'Small Fella'), '2025-06-10 14:00:00', 45, 'Parklands', 'open')
+            `);
+            
