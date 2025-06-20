72e926210b9dcb03dd7af8d3c3b915381ea2b5f0
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:17:46 +0000

    [automatic commit] 2025-06-20 12:17:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -28,5 +28,5 @@ let db;
           username VARCHAR(255) UNIQUE NOT NULL,
           email VARCHAR(255) UNIQUE NOT NULL,
           password_hash VARCHAR(255) NOT NULL,
-          
+          role ENUM('owner', 'walker') NOT NULL
         )`);
