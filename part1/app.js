647f4290f667a01abbcb20299e755753b5cfff26
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 12:42:46 +0000

    [automatic commit] 2025-06-20 12:42:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -145,5 +145,13 @@ app.get('api/walkreqests/open', async (req, res) => {
             SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id = d.dog_id
-            JOIN Users u on d.owner_id = u.user_id`)
+            JOIN Users u on d.owner_id = u.user_id
+            WHERE wr.status = 'open'
+            `);
+            res.json(requests);
+        }
+        catch (err) {
+            console.error('Error fetching open walk requests:', err);
+            res.status(500).json({ error: 'Failed to fetch open walk requests' });
+        }
     })
