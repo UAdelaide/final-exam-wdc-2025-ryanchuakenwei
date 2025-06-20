84baef7e12383a167f987feebc401c1145e1950f
Ryan Chua <a1898548@adelaide.edu.au>
2025-06-20 11:36:51 +0000

    [automatic commit] 2025-06-20 11:36:51 UTC

--- part1/insertDataQuestion5.sql
+++ part1/insertDataQuestion5.sql
@@ -4,4 +4,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('joshua')
+('joshuawalker',)
